1e47a52ea0a0e468fc3f3b46dba8e44c0ed6da73
kimiaf1998 <kajalafshari@yahoo.com>
2022-10-19 14:57:17 -0700

    MVC added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(name: "Rob Blue", email: "rob.pj@example.com").save!
+User.create(name: "William Xu", email: "willi.pj@example.com").save!
+
