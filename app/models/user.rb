68f4c808220087ea983d17a29e35263052316ca6
kimiaf1998 <kajalafshari@yahoo.com>
2022-10-21 10:27:10 -0700

    added user validation in user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   has_many :comment
 
   validates :name, presence: true
-  validates :user_name, presence: true
-  validates :email, presence: true
+  validates :user_name, presence: true, length: { minimum: 10 }
+  validates :email, presence: true, format: { with: /^(.+)@(.+)$/, message: "Email invalid"  },
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 4, maximum: 254 }
+
 
   # add email regex later , min chars for usr name
 
