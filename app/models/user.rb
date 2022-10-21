3e7239cefdb9ac4a0c478ca4698ac943b4a398eb
kimiaf1998 <kajalafshari@yahoo.com>
2022-10-21 11:19:51 -0700

    added user form validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,10 @@ class User < ApplicationRecord
   has_many :post
   has_many :comment
 
-  validates :name, presence: true
-  validates :user_name, presence: true, length: { minimum: 10 }
-  validates :email, presence: true, format: { with: /^(.+)@(.+)$/, message: "Email invalid"  },
+  validates :name, presence: true, format: { with: /\A^[a-zA-Z]+$\z/i, message: "Invalid Name"  }
+  validates :user_name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5 }
+  validates :email, presence: true, format: { with: /\A^(.+)@(.+)$\z/i, message: "Invalid Email Address"  },
             uniqueness: { case_sensitive: false },
             length: { minimum: 4, maximum: 254 }
-
-
-  # add email regex later , min chars for usr name
 
 end
