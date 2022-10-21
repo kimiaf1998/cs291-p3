fd9678ffba5959c830b4896773235f5b81af5673
kimiaf1998 <kajalafshari@yahoo.com>
2022-10-20 20:32:40 -0700

    implemented new user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,28 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :user_name, :email)
+  end
 
   def show
     @user = User.find_by_id(params[:id]) or not_found
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  get 'comments/index'
-  get 'posts/index'
-  get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "users#index"
 
-  get "/users", to: "users#index"
+  # get "/users", to: "users#index"
+  #
+  # get "/users/:id", to: "users#show"
 
-  get "/users/:id", to: "users#show"
+  #####  Why??????
+  # get "/users/new", to: "users#new"
+  #
+  # post "/users", to:"users#create"
+
+  #####
+  resources :users
 end
